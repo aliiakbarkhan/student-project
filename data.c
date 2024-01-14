9ed357aad18269904d71e18e6a01806de1fee3c8
Ali Akbar Khan <56528027+ali363@users.noreply.github.com>
2024-01-14 09:16:31 +0530

    Delete data.c

--- data.c
+++
@@ -1,8 +0,0 @@
- char a[20],b[4];
-    int c[8];
-    printf("Enter Name:");
-    scanf("%s",&a);
-    printf("Enter Branch:");
-    scanf("%s",&b);
-    printf("Enter Roll No:");
-    scanf("%s",&c);
